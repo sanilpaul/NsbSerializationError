5e1cea7853f167116485418688aabd0b713e931b
Sanil Paul <sanilpaul@gmail.com>
2021-02-24 19:41:50 -0500

    git ignore

---
+++ simple_sqltransport_6_netcoreapp2_1/Receiver/MyEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyEventHandler : IHandleMessages<MyEvent>
+{
+    static ILog log = LogManager.GetLogger<MyEventHandler>();
+
+    public async Task Handle(MyEvent eventMessage, IMessageHandlerContext context)
+    {
+        log.Info($"Hello from {nameof(MyEventHandler)}");
+        await context.SendLocal(new MyCommand { MyEvent = eventMessage });
+    }
+}
---
+++ simple_sqltransport_6_netcoreapp2_1/Receiver/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlServer.SimpleReceiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleReceiver");
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+        var connection = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100";
+        transport.ConnectionString(connection);
+        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+
+        endpointConfiguration.EnableInstallers();
+
+        SqlHelper.EnsureDatabaseExists(connection);
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.WriteLine("Waiting for message from the Sender");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
